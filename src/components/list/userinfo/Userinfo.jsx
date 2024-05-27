86d64e1fed4d17be33e292c537443902fbd0c665
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-05-27 23:17:50 +0530

    commiting staged changes

---
+++ src/components/list/userinfo/Userinfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./userinfo.css";
+
+const Userinfo = () => {
+  return (
+    <div className="userInfo">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <p>John Doe</p>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Userinfo;
