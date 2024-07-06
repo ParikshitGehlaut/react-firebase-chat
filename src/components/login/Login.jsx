3b89c05bba2c40c55a1a19e5e32cfd22f2b6ee69
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-07-06 15:55:56 +0530

    update

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./login.css";
 const Login = () => {
-  return <div className="login"></div>;
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: "",
+  });
+
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+  return (
+    <div className="login">
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form action="">
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="separator"></div>
+      <div className="item">
+        <h2>Create an Account</h2>
+        <form action="">
+          <label htmlFor="file">
+            <img src={avatar.url || "./avatar.png"} alt="" />
+            Upload an Image
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
+          <input type="text" name="username" placeholder="Username" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
